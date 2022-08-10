6970ede422e339e7c452456dced04bfed4a6c1d6
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2022-08-10 16:29:49 +0000

    fixed the damn thing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generate(){
-    var input = document.getElementById("input").value;
+    var input = document.getElementById("inputUrl").value;
     var url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`;
 
     var qrCode = document.getElementById("img");
