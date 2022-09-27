763fe9df3a8752a51868d4f31b3a1f293188b971
Pavan B <83314733+ValiantWind@users.noreply.github.com>
2022-09-27 19:09:17 +0000

    Reverted QoL because it didn't work

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
- window.onload = () => {
-        currentURL = window.location.href;
-        lowerCaseURL = currentURL.toLowerCase();
-        if (currentURL != lowerCaseURL) {
-            location.replace(lowerCaseURL);
-  }
-};
 function generate(){
     var inputUrl = document.getElementById("inputUrl").value;
 		var inputColor = document.getElementById("inputColor").value;
