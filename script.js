037491567de3fc27c8ab84b2fef0186448449772
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2025-03-03 21:54:25 -0800

    Pushing another fix

--- script.js
+++ script.js
@@ -123,3 +123,16 @@ function generate() {
         downloadImage(imageUrl, fileFormat);
     });
 }
+
+function validateUrlInput(url) {
+	const pattern = new RegExp(
+	  '^(https?:\\/\\/)?' + // protocol
+		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+		'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+		'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+		'(\\#[-a-z\\d_]*)?$', // fragment locator
+	  'i'
+	);
+	return pattern.test(str);
+  }
